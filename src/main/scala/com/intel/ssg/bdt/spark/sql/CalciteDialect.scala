6571c000414e5aee109436ba5e13378febf08891
Daoyuan Wang <daoyuan.wang@intel.com>
2015-09-06 00:13:12 -0700

    add license and simple case

--- src/main/scala/com/intel/ssg/bdt/spark/sql/CalciteDialect.scala
+++ src/main/scala/com/intel/ssg/bdt/spark/sql/CalciteDialect.scala
@@ -1,15 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intel.ssg.bdt.spark.sql
 
 import org.apache.calcite.sql.SqlNode
 import org.apache.calcite.tools.Frameworks
+import org.apache.spark.Logging
 import org.apache.spark.sql.catalyst.{SqlParser, ParserDialect}
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 
 import scala.util.Try
 
-class CalciteDialect extends ParserDialect {
+class CalciteDialect extends ParserDialect with Logging {
   override def parse(sqlText: String): LogicalPlan = {
-    getLogicalPlan(sqlText).getOrElse(sqlParser.parse(sqlText))
+    getLogicalPlan(sqlText).getOrElse({
+      log.warn("Calcite parse/transform failed")
+      if (CalciteConf.strictMode) {
+        sys.error("Parse failed.")
+      } else {
+        sqlParser.parse(sqlText)
+      }
+    })
   }
 
   @transient protected val sqlParser = new SqlParser
@@ -19,10 +43,10 @@ class CalciteDialect extends ParserDialect {
     val tree: Option[SqlNode] =
       Try(Some(Frameworks.getPlanner(planner).parse(sqlText))).getOrElse(None)
     if (tree.isEmpty) {
-      println("failed with Calcite parser, falling back")
+      log.warn("failed with Calcite parser.")
       None
     } else {
-      println("Calcite parsing passed, start to transform")
+      log.info("Calcite parsing passed, start to transform.")
       Try(Some(CatalystTransformer.sqlNodeToPlan(tree.get))).getOrElse(None)
     }
   }
--- src/main/scala/com/intel/ssg/bdt/spark/sql/CatalystTransformer.scala
+++ src/main/scala/com/intel/ssg/bdt/spark/sql/CatalystTransformer.scala
@@ -1,15 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intel.ssg.bdt.spark.sql
 
-import org.apache.calcite.sql.{SqlKind, SqlNode}
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.calcite.sql._
+import org.apache.spark.Logging
+import org.apache.spark.sql.catalyst.analysis._
+import org.apache.spark.sql.catalyst.expressions.NamedExpression
+import org.apache.spark.sql.catalyst.plans.logical._
 
-private[sql] object CatalystTransformer {
-  def sqlNodeToPlan(sqlNode: SqlNode): LogicalPlan = sqlNode.getKind match {
-    case SqlKind.SELECT =>
-      println("hehe")
-      sys.error("a")
+private[sql] object CatalystTransformer extends Logging {
+  def sqlNodeToPlan(sqlNode: SqlNode): LogicalPlan = sqlNode match {
+    case ss: SqlSelect =>
+      val from = nodeToRelation(ss.getFrom)
+      Project(transformSelectList(ss.getSelectList), from)
     case _ =>
-      println("failed to transform")
       sys.error("unsupported nodes")
+  }
+
+  def transformSelectList(sl: SqlNodeList): Seq[NamedExpression] = {
+    sl.getList.toArray.toSeq.asInstanceOf[Seq[SqlNode]].map {
+      case si: SqlIdentifier => UnresolvedAttribute(si.toString)
+      case _ => sys.error("TODO")
+    }
+  }
+
+  def nodeToRelation(sqlNode: SqlNode): LogicalPlan = sqlNode match {
+    case relation: SqlIdentifier =>
+      UnresolvedRelation(relation.names.toArray.toSeq.asInstanceOf[Seq[String]], None)
+    case _ =>
+      sys.error("TODO")
   }
 }
