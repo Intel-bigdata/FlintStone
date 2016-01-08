5615da64fbb0d932cc04be9cf269c7e7523a255f
happycherry <chao.c.zhang@intel.com>
2016-01-08 15:44:54 +0800

    modify 0912 case gf

--- src/test/scala/org/apache/spark/sql/hive/execution/HiveCompSuite.scala
+++ src/test/scala/org/apache/spark/sql/hive/execution/HiveCompSuite.scala
@@ -110,11 +110,11 @@ class HiveCompSuite extends HiveCompatibilitySuite {
     "0889",
     "0890",
     "1032",
-    "1034"
+    "1034",
     // order by results diff
     "0930",
     "0932",
-    "0934",
+    "0934"
   )
 
   /**
@@ -122,6 +122,7 @@ class HiveCompSuite extends HiveCompatibilitySuite {
    * blacklist are implicitly marked as ignored.
    */
   override def whiteList = Seq(
+    "0912"
     //for nist
     /*"0000",
     "0001",
